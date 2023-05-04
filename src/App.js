3aaa02b91ba526da28eda4d567de5230da55184a
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-05-04 22:55:30 +0200

    some progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/reset.css'
 import './styles/index.css'
-
+import {useState} from "react"
 import initialStoreItems from './store-items'
 
 /*
@@ -14,24 +14,51 @@ Here's what a store item should look like
 What should a cart item look like? 🤔
 */
 
-console.log(initialStoreItems)
+const initialCartItems = []
 
 export default function App() {
   // Setup state here...
+
+  const [storeItems, renderStoreItems] = useState(initialStoreItems)
+
+  const [cartItems, fillCartItems] = useState(initialCartItems)
+  console.log(storeItems, cartItems)
+
+  const clickEvent = (item) => {
+    
+    fillCartItems([...cartItems, item])
+    
+  }
+  console.log(cartItems)
 
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Wrtite some code here... */}
+          {storeItems.map((item, index) => (
+            <li key = {index}>
+              <div className = "store--item-icon">
+                <img src= {`/assets/icons/${item.id}.svg`} alt={`${item.name}`} />                
+              </div>
+              <button onClick = {() => clickEvent(item)}>Add to cart</button>
+            </li>
+          ))}
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Wrtite some code here... */}
+            {cartItems.map((item, index) => (
+              <li key = {index}>
+                <img className = "cart--item-icon" src={`assets/icons/${item.id}.svg`} alt={`${item.name}`}/>
+                <p>{item.name}</p>
+                <button className='quantity-btn remove-btn center'>-</button>
+                <span className = 'quantity-text center'>1</span>
+                <button className='quantity-btn add-btn center'>+</button>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="total-section">
